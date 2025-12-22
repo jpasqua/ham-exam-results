84c05a1fbdf1b54e1b046ec50335b9f3e38c4bd4
Joe Pasqua <joe@noroomattheinn.org>
2025-12-22 15:33:23 -0800

    jpg -> png

--- config.py
+++ config.py
@@ -52,7 +52,7 @@ BRANDING_PROFILES = {
         ]
     },
     'NZ3C': {
-        'logo_path': 'images/NZ3C/NZ3CLogo.jpg',
+        'logo_path': 'images/NZ3C/NZ3CLogo.png',
         'callsign': 'NZ3C',
         'site_name': 'NZ3C Exam Results',
         'subtitle': 'For convenience only, not part of ExamTools.org',
