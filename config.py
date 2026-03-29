5f69ba4cf2ae670a42450be250ff9a5c7212a770
Joe Pasqua <joe@noroomattheinn.org>
2026-03-29 11:07:41 -0700

    Add new team, TARS

--- config.py
+++ config.py
@@ -87,6 +87,18 @@ BRANDING_PROFILES = {
             {'size': '270x270', 'href': '/static/images/NZ3C/mstile-270x270.png'}
         ]
     },
+    'TARS': {
+        'logo_path': 'images/TARS/TARS_Logo.jpg',
+        'callsign': 'TARS',
+        'site_name': 'TARS Exam Results',
+        'subtitle': 'For convenience only, not part of ExamTools.org',
+        'favicons': [
+            {'size': '32x32', 'href': '/static/images/TARS/favicon-32x32.png'},
+            {'size': '192x192', 'href': '/static/images/TARS/favicon-192x192.png'},
+            {'size': '180x180', 'href': '/static/images/TARS/apple-touch-icon.png'},
+            {'size': '270x270', 'href': '/static/images/TARS/mstile-270x270.png'}
+        ]
+    },
     'WAVE': {
         'logo_path': 'images/WAVE/WAVELogo.png',
         'callsign': 'WAVE',
